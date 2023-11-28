b47a5042679d5a8d8b1403848db5c9a065891a55
Ali Mohsen <ali_m_2001@hotmail.com>
2023-11-28 21:16:19 +0200

    add simple model for control logic unit

---
+++ src/control_logic.v
@@ -0,0 +1,36 @@
+module control (
+    output reg INT,
+    input INTA,
+
+    input [7:0] IRR,
+    input [7:0] priority,
+    output reg fromControlLogic_toPriorityResolver
+);
+
+wire isIntrupt;
+integer numberOfAck = 0;
+assign isIntrupt = |(IRR);
+
+
+always @(negedge INTA) begin
+    if (numberOfAck == 2) begin
+        numberOfAck <= 0;
+    end
+    numberOfAck <= numberOfAck + 1;
+end
+
+
+always @(*) begin
+    
+    if (isIntrupt && numberOfAck==0) begin
+        INT <= 1;
+    end
+
+    if (numberOfAck == 1) 
+        fromControlLogic_toPriorityResolver <= 1;
+    else 
+        fromControlLogic_toPriorityResolver <= 0;
+
+end
+    
+endmodule

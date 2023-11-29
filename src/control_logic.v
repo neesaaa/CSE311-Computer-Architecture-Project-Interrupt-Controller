97105da54eaaed4d9d96e45ccc1e9cd9894fd93f
neesaaa <131257593+neesaaa@users.noreply.github.com>
2023-11-30 01:20:52 +0200

    buffer module agdd 7aga

--- src/control_logic.v
+++ src/control_logic.v
@@ -36,21 +36,23 @@ end
     
 endmodule
 module DataBusBuffer (    
-  input wire reset_n,      //negative edge trigger     
-  input wire direction,    // (0: write to PIC, 1: read from PIC)
-  inout wire [7:0] dataBus 
+  input wire direction,    //1 read from pc 0 write on pc
+  input wire [7:0] from_pc,
+  input wire [7:0] from_pic,
+  output wire [7:0] Rxdata,
+  output reg [7:0] Txdata,
 );
 
   reg [7:0] buffer;         
 
   
   always @(*) begin
-    if (~reset_n) begin
-      buffer <= 8'bzzzzzzzz; 
-    end else if (~direction) begin
-      buffer <= dataBus;     // writing
-    end
+    if (direction) begin
+    Txdata <= 8'bzzzzzzzz; 
+    buffer <= from_pc;
+    end      
   end
-  assign dataBus = (direction) ? buffer : 8'bz; // reading
+  assign Rxdata = buffer; //reading
+  assign Txdata = from_pic;
 
 endmodule

bc52bbd201087b9941663e407397bba7a6ab12c6
neesaaa <131257593+neesaaa@users.noreply.github.com>
2023-11-29 22:17:31 +0200

    buffer module

--- src/control_logic.v
+++ src/control_logic.v
@@ -35,3 +35,22 @@ always @(*) begin
 end
     
 endmodule
+module DataBusBuffer (    
+  input wire reset_n,      //negative edge trigger     
+  input wire direction,    // (0: write to PIC, 1: read from PIC)
+  inout wire [7:0] dataBus 
+);
+
+  reg [7:0] buffer;         
+
+  
+  always @(*) begin
+    if (~reset_n) begin
+      buffer <= 8'bzzzzzzzz; 
+    end else if (~direction) begin
+      buffer <= dataBus;     // writing
+    end
+  end
+  assign dataBus = (direction) ? buffer : 8'bz; // reading
+
+endmodule

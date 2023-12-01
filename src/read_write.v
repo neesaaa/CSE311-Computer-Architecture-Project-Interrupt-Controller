24191e985fa72307adab4cb8f8754d1009defaa0
AmrEssamMahmoud <amressamanwer295@gmail.com>
2023-12-01 14:12:53 +0200

    initial design for read write

---
+++ src/read_write.v
@@ -0,0 +1,12 @@
+module Read_write_logic (
+    RD,WR,A0,CS,WR_out,RD_out,A0_out 
+);
+input RD,WR,A0,CS;
+output RD_out,RD_out,A0_out ;
+assign RD_out=(~CS)?RD:1 ;
+assign WR_out=(~CS)?WR:1 ;
+assign A0_out=A0;
+
+
+    
+endmodule

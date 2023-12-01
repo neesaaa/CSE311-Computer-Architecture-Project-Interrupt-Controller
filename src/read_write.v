273e004bc7c77a506210a39cff4dc7f5deadf51e
AmrEssamMahmoud <amressamanwer295@gmail.com>
2023-12-01 18:40:40 +0200

    correcting syntax error and compiled successflly

--- src/read_write.v
+++ src/read_write.v
@@ -4,7 +4,7 @@ module Read_write_logic (
 input RD,WR,A0,CS;
 output RD_out,A0_out,WR_out;
 assign RD_out=(~CS)?RD:1 ;
-assign WR_out=(~CS)?WR:1 ;
+assign WR_out=(~CS )?WR:1 ;
 assign A0_out=A0;
 
 

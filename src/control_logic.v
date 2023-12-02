b0d245f811176d6f02a03338143cc243cdf68a39
neesaaa <131257593+neesaaa@users.noreply.github.com>
2023-12-02 15:56:58 +0200

    sb77

--- src/control_logic.v
+++ src/control_logic.v
@@ -35,14 +35,3 @@ always @(*) begin
 end
     
 endmodule
-module DataBusBuffer (
-    inout [7:0] data, //internal bus when send ** output **  or bus from pc when read so **input** 
-    input direction,  //1>>sending to pc
-    input [7:0] Rx_data, //recieved data
-    input [7:0] Tx_Data //sent data
-);
- 
-assign data = direction ? Tx_Data : 8'bZ;
-assign Rx_Data = data;
-
-endmodule
